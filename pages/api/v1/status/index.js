41966db34830a70d082ae104266a55ae82d8e675
Evandro Costa <evandro.souzac@gmail.com>
2023-12-18 23:04:22 -0300

    Finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,34 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS SUM;");
-  console.log(result.rows);
-  response.status(200).json({ status: "Endpoint status funcionando." });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersion = await database.query("SHOW server_version;");
+
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnections = await database.query({
+    text: "SELECT COUNT(*)::INTEGER AS opened_connections FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const results = {
+    max_connections: parseInt(databaseMaxConnections.rows[0].max_connections),
+    opened_connections: databaseOpenedConnections.rows[0].opened_connections,
+    version: databaseVersion.rows[0].server_version,
+  };
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        max_connections: results.max_connections,
+        opened_connections: results.opened_connections,
+        version: results.version,
+      },
+    },
+  });
 }
 
 export default status;
