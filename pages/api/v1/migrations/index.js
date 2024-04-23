a23111f95a677ac9d64c91d60fa7fddf269de183
Evandro Costa <evandro.souzac@gmail.com>
2024-04-23 15:52:07 -0300

    Fix not allowed methods when access migrations endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,39 +3,51 @@ import { join } from "node:path";
 import database from "infra/database";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
+  const allowedMethods = ["GET", "POST"];
 
-  const defaultMigrationsOptions = {
-    dbClient: dbClient,
-    databaseUrl: process.env.DATABASE_URL,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-    noLock: true,
-  };
+  if (allowedMethods.includes(request.method)) {
+    let dbClient;
+    try {
+      dbClient = await database.getNewClient();
+      const defaultMigrationsOptions = {
+        dbClient: dbClient,
+        databaseUrl: process.env.DATABASE_URL,
+        dryRun: true,
+        dir: join("infra", "migrations"),
+        direction: "up",
+        verbose: true,
+        migrationsTable: "pgmigrations",
+        noLock: true,
+      };
 
-  if (request.method === "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
-    await dbClient.end();
-    return response.status(200).json(pendingMigrations);
-  }
+      if (request.method === "GET") {
+        const pendingMigrations = await migrationRunner(
+          defaultMigrationsOptions,
+        );
+        return response.status(200).json(pendingMigrations);
+      }
 
-  if (request.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationsOptions,
-      dryRun: false,
-    });
+      if (request.method === "POST") {
+        const migratedMigrations = await migrationRunner({
+          ...defaultMigrationsOptions,
+          dryRun: false,
+        });
 
-    await dbClient.end();
+        if (migratedMigrations.length > 0) {
+          return response.status(201).json(migratedMigrations);
+        }
 
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations);
+        return response.status(200).json(migratedMigrations);
+      }
+    } catch (error) {
+      console.error(error);
+      throw error;
+    } finally {
+      await dbClient.end();
     }
-
-    return response.status(200).json(migratedMigrations);
+  } else {
+    return response.status(405).json({
+      error: `Method ${request.method} not allowed.`,
+    });
   }
-
-  return response.status(405).end();
 }
